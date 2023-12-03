1207a871616feaa498edef34d7a6e052759e839c
MN <57828955+MNgaminhhh@users.noreply.github.com>
2023-12-03 23:41:49 +0700

    add usercontroller and template user

--- src/main/java/com/hcmute/alohcmute/controllers/UserController.java
+++ src/main/java/com/hcmute/alohcmute/controllers/UserController.java
@@ -1,21 +1,13 @@
 package com.hcmute.alohcmute.controllers;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.hcmute.alohcmute.entity.User;
-import com.hcmute.alohcmute.service.UserService;
-
 @Controller
 public class UserController {
-
-    private UserService userService;
-
     @GetMapping("/users")
     public String showUsers(Model model) {
-        return "users";
+        return "user/page";
     }
 }
