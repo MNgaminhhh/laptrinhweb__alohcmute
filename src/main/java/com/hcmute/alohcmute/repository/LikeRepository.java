a6cb313aef7aacb61921981c13b97fcb23e52249
MN <57828955+MNgaminhhh@users.noreply.github.com>
2023-12-14 09:55:00 +0700

    upadte like and something

--- src/main/java/com/hcmute/alohcmute/repository/LikeRepository.java
+++ src/main/java/com/hcmute/alohcmute/repository/LikeRepository.java
@@ -1,10 +1,12 @@
 package com.hcmute.alohcmute.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hcmute.alohcmute.entity.Like;
 
-
 public interface LikeRepository extends JpaRepository<Like, Long> {
-    // Các truy vấn cụ thể có thể được thêm ở đây nếu cần
+    Optional<Like> findByPost_PostIdAndUser_UserId(Long postId, Long userId);
+    Optional<Like> findByUser_UserIdAndPost_PostId(Long userId, Long postId);
 }
